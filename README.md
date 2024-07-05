065d06bfab757cc4183cef3edd1d087e57adb122
pilusx <poteka@wp.pl>
2024-07-05 15:58:36 +0200

    The grid search

--- README.md
+++ README.md
@@ -75,6 +75,7 @@ This is a repository containing my solutions to Hackerrank problems.
 | [Super maximum cost queries](https://www.hackerrank.com/challenges/maximum-cost-queries/problem) | 60 |
 | [Swap nodes [Algo]](https://www.hackerrank.com/challenges/swap-nodes-algo/problem) | 40 |
 | [The coin change problem](https://www.hackerrank.com/challenges/three-month-preparation-kit-coin-change/problem) | 100 |
+| [The grid search](https://www.hackerrank.com/challenges/the-grid-search/problem) | 30 |
 | [The maximum subarray](https://www.hackerrank.com/challenges/three-month-preparation-kit-maxsubarray/problem) | 100 |
 | [The strange function](https://www.hackerrank.com/challenges/the-strange-function/problem) | 15 | 
 | [The time in words](https://www.hackerrank.com/challenges/the-time-in-words/problem) | 25 |
---
+++ TheGridSearch.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+    static boolean check(int i, int j, List<String> G, List<String> P) {
+        int n1 = P.size(), m1 = P.get(0).length();
+        for(int u = 0; u < n1; u++) {
+            for(int v = 0; v < m1; v++) {
+                if(G.get(i+u).charAt(j+v) != P.get(u).charAt(v)) return false;
+            }
+        }
+        return true;
+    }
+
+
+    /*
+     * Complete the 'gridSearch' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY G
+     *  2. STRING_ARRAY P
+     */
+
+    public static String gridSearch(List<String> G, List<String> P) {
+        int n = G.size(), m = G.get(0).length();
+        int n1 = P.size(), m1 = P.get(0).length();
+        
+        for(int i = 0; i <= n-n1; i++) {
+            for(int j = 0; j <= m-m1; j++) {
+                if(check(i, j, G, P)) return "YES";
+            }
+        }
+        return "NO";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int R = Integer.parseInt(firstMultipleInput[0]);
+
+                int C = Integer.parseInt(firstMultipleInput[1]);
+
+                List<String> G = IntStream.range(0, R).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int r = Integer.parseInt(secondMultipleInput[0]);
+
+                int c = Integer.parseInt(secondMultipleInput[1]);
+
+                List<String> P = IntStream.range(0, r).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String result = Result.gridSearch(G, P);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
