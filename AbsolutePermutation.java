11fde72dc9a2ccdedeab5d769fc639b445d8e393
pilusx <poteka@wp.pl>
2024-07-13 17:16:58 +0200

    Absolute permutation

---
+++ AbsolutePermutation.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'absolutePermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     */
+
+    public static List<Integer> absolutePermutation(int n, int k) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> used = new HashSet<>();
+        for(int i = 1; i <= n; i++) {
+            int u = i - k, v = i + k;
+            if(u > 0 && !used.contains(u)) {
+                used.add(u);
+                result.add(u);
+            } else if (v <= n && !used.contains(v)) {
+                used.add(v);
+                result.add(v);
+            } else {
+                return Arrays.asList(-1);
+            }
+        }
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> result = Result.absolutePermutation(n, k);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This is a repository containing my solutions to Hackerrank problems.
 | Problem (Link) | Points |
 | --- | ---|
 | [3D surface area](https://www.hackerrank.com/challenges/3d-surface-area/problem) | 30 |
+| [Absolute permutation](https://www.hackerrank.com/challenges/absolute-permutation/problem) | 40 |
 | [And xor or](https://www.hackerrank.com/challenges/and-xor-or/problem) | 70 |
 | [Array and simple queries](https://www.hackerrank.com/challenges/array-and-simple-queries/problem) | 80 |
 | [Array manipulation](https://www.hackerrank.com/challenges/crush/problem) | 60 |
