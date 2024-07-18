8c104729355a567168ec40a96fe8c712b2cb09df
pilusx <poteka@wp.pl>
2024-07-18 19:43:52 +0200

    Funny string

---
+++ FunnyString.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+        String s2 = new StringBuilder(s).reverse().toString();
+        for(int i = 0; i + 1 < s.length(); i++) {
+            if(Math.abs(s.charAt(i) - s.charAt(i+1)) != Math.abs(s2.charAt(i) - s2.charAt(i+1))) 
+                return "Not Funny";
+        }
+        return "Funny";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
--- README.md
+++ README.md
@@ -37,6 +37,7 @@ This is a repository containing my solutions to Hackerrank problems.
 | [Find the running median](https://www.hackerrank.com/challenges/three-month-preparation-kit-find-the-running-median/problem) | 100 |
 | [Floyd: City of blinding lights](https://www.hackerrank.com/challenges/three-month-preparation-kit-floyd-city-of-blinding-lights/problem) | 100 |
 | [Forming a magic square](https://www.hackerrank.com/challenges/three-month-preparation-kit-magic-square-forming/problem) | 100 |
+| [Funny string](https://www.hackerrank.com/challenges/funny-string/problem) | 25 |
 | [Game of two stacks](https://www.hackerrank.com/challenges/game-of-two-stacks/problem) | 30 |
 | [Gena playing hanoi](https://www.hackerrank.com/challenges/three-month-preparation-kit-gena/problem) | 100 |
 | [Hackerland radio transmitters](https://www.hackerrank.com/challenges/three-month-preparation-kit-hackerland-radio-transmitters/problem) | 100 |
